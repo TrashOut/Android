47257ae1451079d5dc811525e1b2676317d7a78b
filip.tomasovych <filip.tomasovych@qbsw.sk>
2018-09-11 11:45:05 +0200

    TRASH-1198 And - Pád aplikácie pri prihlasení cez FB

--- app/src/main/java/me/trashout/fragment/ProfileEditFragment.java
+++ app/src/main/java/me/trashout/fragment/ProfileEditFragment.java
@@ -1,26 +1,26 @@
 /*
- * TrashOut is an environmental project that teaches people how to recycle 
+ * TrashOut is an environmental project that teaches people how to recycle
  * and showcases the worst way of handling waste - illegal dumping. All you need is a smart phone.
- *  
- *  
+ *
+ *
  * There are 10 types of programmers - those who are helping TrashOut and those who are not.
- * Clean up our code, so we can clean up our planet. 
+ * Clean up our code, so we can clean up our planet.
  * Get in touch with us: help@trashout.ngo
- *  
+ *
  * Copyright 2017 TrashOut, n.f.
- *  
+ *
  * This file is part of the TrashOut project.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * See the GNU General Public License for more details: <https://www.gnu.org/licenses/>.
  */
 
@@ -193,24 +193,21 @@ public class ProfileEditFragment extends BaseFragment implements IProfileFragmen
     }
 
     private void setupUserProfile(User user) {
-        if (user.getImage() != null) {
-            if (user.getImage() != null) {
-                StorageReference mImageRef = FirebaseStorage.getInstance().getReferenceFromUrl(user.getImage().getFullStorageLocation());
-                GlideApp.with(this)
-                        .asBitmap()
-                        .load(mImageRef)
-                        .placeholder(R.drawable.ic_image_placeholder_rectangle)
-                        .into(new BitmapImageViewTarget(profileEditImage) {
-                            @Override
-                            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                                RoundedBitmapDrawable circularBitmapDrawable =
-                                        RoundedBitmapDrawableFactory.create(getActivity().getResources(), resource);
-                                circularBitmapDrawable.setCircular(true);
-                                profileEditImage.setImageDrawable(circularBitmapDrawable);
-                            }
-                        });
-
-            }
+        if (user.getImage() != null && !TextUtils.isEmpty(user.getImage().getFullStorageLocation())) {
+            StorageReference mImageRef = FirebaseStorage.getInstance().getReferenceFromUrl(user.getImage().getFullStorageLocation());
+            GlideApp.with(this)
+                    .asBitmap()
+                    .load(mImageRef)
+                    .placeholder(R.drawable.ic_image_placeholder_rectangle)
+                    .into(new BitmapImageViewTarget(profileEditImage) {
+                        @Override
+                        public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                            RoundedBitmapDrawable circularBitmapDrawable =
+                                    RoundedBitmapDrawableFactory.create(getActivity().getResources(), resource);
+                            circularBitmapDrawable.setCircular(true);
+                            profileEditImage.setImageDrawable(circularBitmapDrawable);
+                        }
+                    });
         }
 
         profileEditFirstName.setText(user.getFirstName());
