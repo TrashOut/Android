ee3aa1b1492ae6e2ebe6fc82f6d6d4f1dcfd8491
filip.tomasovych <filip.tomasovych@qbsw.sk>
2019-04-11 11:53:06 +0200

    TRASH-1245 change gps formatting

--- app/src/main/java/me/trashout/utils/PositionUtils.java
+++ app/src/main/java/me/trashout/utils/PositionUtils.java
@@ -1,26 +1,26 @@
 /*
- * TrashOut is an environmental project that teaches people how to recycle 
+ * TrashOut is an environmental project that teaches people how to recycle
  * and showcases the worst way of handling waste - illegal dumping. All you need is a smart phone.
- *  
- *  
+ *
+ *
  * There are 10 types of programmers - those who are helping TrashOut and those who are not.
- * Clean up our code, so we can clean up our planet. 
+ * Clean up our code, so we can clean up our planet.
  * Get in touch with us: help@trashout.ngo
- *  
+ *
  * Copyright 2017 TrashOut, n.f.
- *  
+ *
  * This file is part of the TrashOut project.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * See the GNU General Public License for more details: <https://www.gnu.org/licenses/>.
  */
 
@@ -64,8 +64,7 @@ public class PositionUtils {
     public static String getFormattedLocation(Context context, double latitude, double longitude) {
 
         if (PreferencesHandler.getUserPreferredLocationFormat(context) == 0) {
-            return String.format(context.getString(R.string.position_formatter), latitude) + ", "
-                    + String.format(context.getString(R.string.position_formatter), longitude);
+            return latitude + ", " + longitude;
         } else {
             return getFormattedLocationInDegree(context, latitude, longitude);
         }
