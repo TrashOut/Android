e97b62fa946be9ef0408c9ddb80df7a8399b6e4d
filip.tomasovych <filip.tomasovych@qbsw.sk>
2019-05-29 13:39:34 +0200

    open register tab after navigating from tutorial

--- app/src/main/java/me/trashout/fragment/StartFragment.java
+++ app/src/main/java/me/trashout/fragment/StartFragment.java
@@ -1,26 +1,26 @@
 /*
- * TrashOut is an environmental project that teaches people how to recycle 
+ * TrashOut is an environmental project that teaches people how to recycle
  * and showcases the worst way of handling waste - illegal dumping. All you need is a smart phone.
- *  
- *  
+ *
+ *
  * There are 10 types of programmers - those who are helping TrashOut and those who are not.
- * Clean up our code, so we can clean up our planet. 
+ * Clean up our code, so we can clean up our planet.
  * Get in touch with us: help@trashout.ngo
- *  
+ *
  * Copyright 2017 TrashOut, n.f.
- *  
+ *
  * This file is part of the TrashOut project.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *  
+ *
  * See the GNU General Public License for more details: <https://www.gnu.org/licenses/>.
  */
 
@@ -36,7 +36,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.firebase.ui.auth.ErrorCodes;
 import com.google.android.gms.common.ConnectionResult;
@@ -259,7 +258,9 @@ public class StartFragment extends BaseFragment implements BaseService.UpdateSer
         if (startDashborad) {
             ((StartActivity) getActivity()).startMainActivity();
         } else {
-            startActivity(BaseActivity.generateIntent(getContext(), LoginFragment.class.getName(), null, MainActivity.class));
+            Bundle args = new Bundle();
+            args.putInt(LoginFragment.EXTRA_SELECTED_TAB, R.id.page_sign_up);
+            startActivity(BaseActivity.generateIntent(getContext(), LoginFragment.class.getName(), args, MainActivity.class));
         }
         finish();
     }
