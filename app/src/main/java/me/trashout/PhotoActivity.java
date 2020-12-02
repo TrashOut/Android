708779d68a517af1d2f1279492f1ae2edb65235d
Jozef Liška <jfox@jfox.sk>
2020-12-02 12:20:28 +0100

    Remove unused import

--- app/src/main/java/me/trashout/PhotoActivity.java
+++ app/src/main/java/me/trashout/PhotoActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.otaliastudios.cameraview.CameraListener;
