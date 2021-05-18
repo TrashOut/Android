79d5ee2d71b2d2241960766c0ef7e8c3f059c626
Jozef Liška <jfox@jfox.sk>
2021-05-18 13:58:22 +0200

    Migrate to Support Library v28.0.0

--- app/src/main/java/me/trashout/utils/BottomNavigationViewHelper.java
+++ app/src/main/java/me/trashout/utils/BottomNavigationViewHelper.java
@@ -27,6 +27,7 @@
 package me.trashout.utils;
 
 import android.annotation.SuppressLint;
+import android.support.design.bottomnavigation.LabelVisibilityMode;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
@@ -37,6 +38,7 @@ import java.lang.reflect.Field;
 public class BottomNavigationViewHelper {
     private static final String TAG = BottomNavigationViewHelper.class.getSimpleName();
 
+    @SuppressLint("RestrictedApi")
     public static void disableShiftMode(BottomNavigationView view) {
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
         try {
@@ -46,7 +48,7 @@ public class BottomNavigationViewHelper {
             shiftingMode.setAccessible(false);
             for (int i = 0; i < menuView.getChildCount(); i++) {
                 BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
-                item.setShiftingMode(false);
+                item.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);;
                 // set once again checked value, so view will be updated
                 item.setChecked(item.getItemData().isChecked());
             }
